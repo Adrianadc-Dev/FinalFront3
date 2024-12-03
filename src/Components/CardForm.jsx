9527529a773643039984d4722a1957dab27c1e91
Adrianadc-Dev <addiaz802@gmail.com>
2024-12-03 18:01:25 -0500

    creacion pagina contact y nav

---
+++ src/Components/CardForm.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import "../styles/index.css"
+const CardForm = ({nombre, email}) => {
+  return (
+    <div className="div">
+        <h3>Gracias {nombre}! te contactaremos cuanto antes via email al correo: {email}</h3>
+    </div>
+  )
+   
+}
+
+export default CardForm
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,58 @@
-import React from "react";
-
+import { useState } from "react";
+import CardForm from "./CardForm";
+import "../styles/index.css"
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [usuario, setUsuario]= useState({
+      nombre: "",
+      email: ""
+  })
+  const [mostrar, setMostrar] = useState(false);
+  const [err, setErr] = useState(false);
+  const handleSubmi =(event)=>{
+      const regexEmail= new RegExp('[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}')
+      event.preventDefault();
+      if(usuario.nombre.trim().length>=5 && regexEmail.test(usuario.email)){
+          setMostrar(true);
+          setErr(false);
+      }else{
+          setErr(true);
+      }
 
-  return (
+  }
+  console.log(usuario);
+return (
+  <>
+    <form className= "form" onSubmit={handleSubmi}>
+      <label>Escribe tu nombre completo</label>
+      <input
+        type="text"
+        value={usuario.nombre}
+        onChange={(event) =>
+          setUsuario({ ...usuario, nombre: event.target.value })
+        }
+      />
+  
+      <label>Email</label>
+      <input
+        type="text"
+        value={usuario.email}
+        onChange={(event) =>
+          setUsuario({ ...usuario, email: event.target.value })
+        }
+      />
+      <button className="button_enviar">Enviar</button>
+      {err ? (
+        <h4 style={{ color: "red" }}>Por favor verifique su informacion nuevamente</h4>
+      ) : null}
+    </form>
     <div>
-      <form>
-      </form>
+    {mostrar ? (
+      <CardForm nombre={usuario.nombre} email={usuario.email}/>
+    ) : null }
     </div>
-  );
-};
+  </>
+);
+}
 
-export default Form;
+export default Form
